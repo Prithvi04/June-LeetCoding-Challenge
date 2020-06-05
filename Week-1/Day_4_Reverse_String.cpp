2250c5ce430fd636d1cd99b658d41c094522c252
Prithviraj N Indi <38948106+Prithvi04@users.noreply.github.com>
2020-06-05 10:54:40 +0530

    Add files via upload

---
+++ Week-1/Day_4_Reverse_String.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i=0,j=s.size()-1;
+        
+        while(i<=j)
+        {
+            char temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;
+            j--;
+        }
+        
+    }
+};
+//The idea is based on oposite directional two pointer approach.
+//Time Complexity O(N) with O(1) extra space
